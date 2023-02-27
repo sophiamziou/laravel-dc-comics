0f69747b732c68ec526406a4c067417ce42623c5
sophiamziou <sofiamziou@gmail.com>
2023-02-27 14:03:47 +0100

    styled buttons

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,19 +1,21 @@
 @extends('layouts.app')
 @section('content')
- <div class="card m-3" style="width: 18rem;">
-    <img src="{{$comic['thumb']}}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">{{$comic['title']}}</h5>
-     </div>
-</div>
-<a href="{{ route('comics.edit', $comic )}}">
-  <button type="button" class="btn btn-primary">EDIT</button>
-</a>
-<div>
-  <form action="{{route('comics.destroy', ['comic' => $comic['id']] )}}" method="POST">
+<div class="d-flex m-3 gap-3">
+  <a href="{{ route('comics.edit', $comic )}}">
+    <button type="button" class="btn btn-primary">EDIT</button>
+  </a>
+  <div>
+    <form action="{{route('comics.destroy', ['comic' => $comic['id']] )}}" method="POST">
       @csrf
       @method('DELETE')
-      <input type="submit" name="" id="" value="cancella">
-  </form>
+      <input class="btn btn-danger" type="submit" name="" id="" value="cancella">
+    </form>
+  </div>
+</div>
+<div class="card m-3" style="width: 18rem;">
+   <img src="{{$comic['thumb']}}" class="card-img-top" alt="...">
+   <div class="card-body">
+     <h5 class="card-title">{{$comic['title']}}</h5>
+   </div>
 </div>
 @endsection
